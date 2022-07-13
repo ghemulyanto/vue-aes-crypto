bcfa2a281ebecf7ad6dab9f68838d6a318758008
ghemulyanto <trimulyanto.tm@gmail.com>
2022-07-13 08:27:16 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCryptojs from 'vue-cryptojs'
 
 Vue.config.productionTip = false
+Vue.use(VueCryptojs)
 
 new Vue({
   render: h => h(App),
